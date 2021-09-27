5eac05c0e7ae7b578d5577195fdbb72bb4eda40f
Inna <inna.kachinskaya13@gmail.com>
2021-09-27 17:07:03 +0300

    Add images.js

---
+++ src/images.js
@@ -0,0 +1,8 @@
+const images = [
+  "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+  "https://lh3.googleusercontent.com/proxy/JqmC3tYrF_QW7n-ErcxtSb9s8ngrrkNaixymrdLDRSBMiOQMCFx-9J1KJVslZnSwGINvMQrtYZzWAgO2ssS9n9QYuodJtrNLu8CYizwKNgb5pff0-y0IPjVI9LrIS0g6Kj4",
+  "https://www.wshu.org/sites/wshu/files/202010/pexels-photo-206359.jpeg",
+  "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
+];
+
+export default images;
