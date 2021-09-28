6ed593eca7fa700db7553b62b3caf2fa87d56ec1
Inna <inna.kachinskaya13@gmail.com>
2021-09-28 13:06:01 +0300

    Refactor images.js

--- src/images.js
+++ src/images.js
@@ -1,8 +1,35 @@
 const images = [
-  "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
-  "https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg",
-  "https://www.wshu.org/sites/wshu/files/202010/pexels-photo-206359.jpeg",
-  "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
+  {
+    id: 1,
+    name: "Volcano",
+    description:
+      "An effusive geological formation that has an outlet (vent, crater, caldera) or cracks, from which hot lava and volcanic gases come to the surface from the interior of the planet, or came earlier.",
+    URL: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Fog",
+    description:
+      "    Atmospheric phenomenon, accumulation of water in the air, formed by the smallest particles of water.",
+    URL: "https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Sunset",
+    description:
+      "Sunset over the horizon (sun, celestial body); time of such a call.",
+    URL: "https://www.wshu.org/sites/wshu/files/202010/pexels-photo-206359.jpeg",
+  },
+
+  {
+    id: 4,
+    name: "Chamomile",
+    description:
+      "A genus of perennial flowering plants of the Asteraceae family, or Asteraceae, unites about twenty species of low odorous grasses that bloom from the first year of life.",
+    URL: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
+  },
 ];
 
 export default images;
